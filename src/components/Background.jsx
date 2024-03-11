800337cbc685a7f2b665753620c69c4b23087468
sajal <Jainsajal519@gmail.com>
2024-03-12 00:06:17 +0530

    Card component designed

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Background() {
   return (
-    <div>
+    <div className=' fixed z-[2] w-full h-screen'>
       <div className='absolute top-[5%] w-full py-10 font-semibold text-zinc-600 text-xl flex justify-center'>Documents.</div>
       <h1 className='absolute top-1/2 left-1/2 text-[13vw] -translate-x-[50%] -translate-y-[50%] leading-none tracking-tighter font-semibold text-zinc-900'>Docs. </h1>
     </div>
---
+++ src/components/Foreground.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Card from './card'
+function Foreground() {
+  return (
+      <div className='absolute z-[3] top-0 left-0 h-screen w-full'>
+       <Card />
+       </div>
+  )
+}
+
+export default Foreground
