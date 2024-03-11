db6aad2bb930e01916998a124bd252009119093f
sajal <Jainsajal519@gmail.com>
2024-03-11 15:20:27 +0530

    Added Navbar

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Background from './components/Background'
+const App = () => {
+  return (
+    <div className=' relative w-full h-screen bg-zinc-800'>
+      <Background />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Background.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Background() {
+  return (
+    <div>
+      <div className='absolute top-[5%] w-full py-10 font-semibold text-zinc-600 text-xl flex justify-center'>Documents.</div>
+      <h1 className='absolute top-1/2 left-1/2 text-[13vw] -translate-x-[50%] -translate-y-[50%] leading-none tracking-tighter font-semibold text-zinc-900'>Docs. </h1>
+    </div>
+  )
+}
+
+export default Background
