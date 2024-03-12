8d180e75b1751ef2676b410d776ab7afe21d8b4a
sajal <Jainsajal519@gmail.com>
2024-03-12 11:12:28 +0530

    Added multiple cards

--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import Card from './card'
 function Foreground() {
+    const data=[
+        {
+            desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            filesize: ".9mb",
+            close: false,
+            tag:{isOpen:true, tagTitle:"Download Now!", tagColor:"blue"}
+        },
+        {
+            desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            filesize: ".9mb",
+            close: false,
+            tag:{isOpen:true, tagTitle:"Download Now!", tagColor:"green"}
+        },
+        {
+            desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            filesize: ".9mb",
+            close: false,
+            tag:{isOpen:true, tagTitle:"Download Now!", tagColor:"green"}
+        }
+    ]
   return (
-      <div className='absolute z-[3] top-0 left-0 h-screen w-full'>
-       <Card />
+      <div className='absolute z-[3] top-0 left-0 h-screen w-full flex gap-5 flex-wrap'>
+       {data.map((item,index)=>(
+        <Card data={item}/>
+       ))}
        </div>
   )
 }
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import { FaRegFileAlt } from "react-icons/fa";
-function Card() {
+import { LuDownload } from "react-icons/lu";
+import { IoClose } from "react-icons/io5";
+function Card({data}) {
   return (
-    <div className='relative w-60 h-72 rounded-[50px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden' >
+    <div className='relative w-60 h-72 rounded-[45px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden' >
        <FaRegFileAlt />
-       <p className='text-sm mt-5 font-semibold leading-tight'>Lorem ipsum dolor sit amet.</p>
-       <div className='footer absolute bottom-0 w-full h-20 bg-white left-0'></div>
+       <p className='text-sm mt-5 font-semibold leading-tight'>{data.desc}</p>
+       <div className='footer absolute bottom-0 w-full left-0'>
+        <div className='flex items-center justify-between px-8 py-3 mb-3'>
+            <h5>{data.filesize}</h5>
+            <span className='w-5 h-5 bg-zinc-600 rounded-full flex items-center justify-center'>
+                {data.close ? <IoClose/>:<LuDownload size=".7rem" color= '#fff'></LuDownload>}
+            </span>
+        </div>
+        {data.tag.isOpen &&(
+        <div className={`tag w-full py-4 ${data.tag.tagColor === "blue" ? "bg-blue-600":"bg-green-600"} flex items-center justify-center`}>
+            <h3 className='text-sm font-semibold'>{data.tag.tagTitle}</h3>
+        </div>
+        )
+        }   
+    </div>
     </div>
   )
 }
